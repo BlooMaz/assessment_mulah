5655b1b6605b1c4fc19fc26894253dfa2a5d0a10
BlooMaz <muhdmuaz121@gmail.com>
2023-07-20 19:36:54 +0800

    new file:   .vscode/launch.json 	modified:   home.html 	modified:   scriptDataRead.js 	modified:   style.css

--- scriptDataRead.js
+++ scriptDataRead.js
@@ -27,6 +27,55 @@ function populateTable(csvData) {
     });
 }
 
+
+// Function to read the CSV file and populate the table
+function processData(csvData) {
+    const tableBody = document.querySelector("#data-processed tbody");
+    tableBody.innerHTML = "";
+
+    const headers = ["Category","Value"]; // First row contains the column headers
+    const dataRows = csvData.slice(1); // Exclude the first row (header) from the data rows
+
+    // Create header row
+    const headerRow = document.createElement("tr");
+    headers.forEach((header) => {
+        const th = document.createElement("th");
+        th.textContent = header;
+        headerRow.appendChild(th);
+    });
+    tableBody.appendChild(headerRow);
+
+    const valueColumn = [];
+    for (const row of rows) {
+        const columns = row.split(',');
+        if (columns.length > 0) {
+          valueColumn.push(parseInt(columns[1]));
+        }
+      }
+    const newRow = document.createElement("tr");
+    const newCell = document.createElement("td");
+    newCell.textContent = "Alpha"
+    newRow.appendChild(newCell);
+    newCell.textContent = valueColumn[4] + valueColumn[19]
+    newRow.appendChild(newCell);
+    tableBody.appendChild(newRow);
+
+    newCell.textContent = "Beta"
+    newRow.appendChild(newCell);
+    newCell.textContent = valueColumn[14] / valueColumn[6]
+    newRow.appendChild(newCell);
+    tableBody.appendChild(newRow);
+
+    newCell.textContent = "Charlie"
+    newRow.appendChild(newCell);
+    newCell.textContent = valueColumn[12] * valueColumn[11]
+    newRow.appendChild(newCell);
+    tableBody.appendChild(newRow);
+
+    
+}  
+
+
 // Function to fetch the CSV file
 function fetchCSVAndPopulateTable() {
     fetch("Table_Input.csv")
@@ -35,6 +84,7 @@ function fetchCSVAndPopulateTable() {
             const rows = csvText.split("\n");
             const csvData = rows.map((row) => row.split(","));
             populateTable(csvData);
+            processData(csvData);
         })
         .catch((error) => console.error("Error fetching CSV:", error));
 }
